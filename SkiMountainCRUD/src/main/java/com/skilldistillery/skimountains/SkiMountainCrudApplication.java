49cb86c1bd598401cea907e05d367798b6a92010
Thomas Webel <Thomaswebel@gmail.com>
2021-03-12 19:30:08 -0500

    Stub Boot/MVC project, link JPA dependency

---
+++ SkiMountainCRUD/src/main/java/com/skilldistillery/skimountains/SkiMountainCrudApplication.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.skimountains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiMountainCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiMountainCrudApplication.class, args);
+	}
+
+}
+ 
